3754f6ae09865d76ed575ce6b66e33f20ba181f3
amir movassaghi <movassaghi6@gmail.com>
2025-02-03 18:51:01 +0330

    Update like_post function to change dynamically the ID of latest post

--- app/search_for_profile.py
+++ app/search_for_profile.py
@@ -56,7 +56,6 @@ def update_data(base_data, **kwargs):
     return updated_data
 
 
-
 # default headers
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:134.0) Gecko/20100101 Firefox/134.0",
@@ -75,39 +74,10 @@ headers = {
 }
 
 
-# Data dictionary to include POST request parameters and Instagram-specific data
-data = {
-    "av": "17841461002400656",
-    "__d": "www",
-    "__user": "0",
-    "__a": "1",
-    "__req": "16",
-    "__hs": "20121.HYP:instagram_web_pkg.2.1...1",
-    "dpr": "1",
-    "__ccg": "MODERATE",
-    "__rev": "1019771082",
-    "__s": "dswjdn:rzn4z9:ahi64c",
-    "__hsi": "7466716840802685629",
-    "__dyn": "7xeUjG1mxu1syaxG4Vp41twpUnwgU7SbzEdF8aUco2qwJyEiw9-1DwUx60p-0LVE4W0qa321Rw8G11wBz81s8hwGxu786a3a1YwBgao6C0Mo2iyo7u3ifK0zEkxe2GewGw9a3614xm0zK5o4q3y261kx-0ma2-azqwt8d-2u2J08O321LwTwKG1pg2fwxyo6O1FwlEcUed6goK2OubK5V89FbxG1oxe6UaUaE2xyVrx6",
-    "__csr": "jMdJgkjnbbk8OkJlid4HsgSZeJZlAnX_GQDajiUxCnFbq_KOGiLHGQAAihBqAFV5hdk-mbGF9AyJemV4XHlbGh2XHhlgVtAzFcx7CBGVU-8LyUx2cCmqRxbACFBmiHgCqi4sBpbAyFkiVHGqmuaAzEhAzECQdGnyUAxdbBV4m4UGaFUlGiaBw04Sr80OUc41zwjolU4C0KX82CEa46qyojC5AgWqahl42Q2WS1Gw9u1mxOqt2EChwIwou2pdXolwik3zB50Xxa2qhPCgO093waa2O1YgdE6-5o0wa0nG0b3BIUC1mxmmRxqfxaH9hU-6Q7rzU5S1xByU-Vk0H42gU55xwwPQoml2187m362CewJ2gCjwjEmwk9EtxXdho790gu0X8eo8Y80Ww1zoE9o0Eqq4QfBG1lhojxp0Bg04RKE4K0w2auaw3bo1IUK09BQGxy2S045E0-pw",
-    "__comet_req": "7",
-    "fb_dtsg": "NAcMbE-9Uwk-r7aJpJ6zdu2mJaCGnzue7rX0xObtX2EN_vU4KShlUiQ:17858449030071790:1738410193",
-    "jazoest": "26297",
-    "lsd": "BLoqaXOI_Q7IhuTVLrwftw",
-    "__spin_r": "1019771082",
-    "__spin_b": "trunk",
-    "__spin_t": "1738480488",
-    "fb_api_caller_class": "RelayModern",
-    "fb_api_req_friendly_name": "PolarisSearchNullStateQuery",
-    "variables": "{}",
-    "server_timestamps": "true",
-    "doc_id": "8568350183286091"
-}
-
-
+data = 'route_urls^[0^]=^%^2Fstories^%^2Fhighlights^%^2F17869897943334651^%^2F^%^3Fr^%^3D1&route_urls^[1^]=^%^2Fstories^%^2Fhighlights^%^2F17981893000342210^%^2F^%^3Fr^%^3D1&route_urls^[2^]=^%^2Fstories^%^2Fhighlights^%^2F17916299836591418^%^2F^%^3Fr^%^3D1&route_urls^[3^]=^%^2Fabugarcia_fishing^%^2Ffollowers^%^2F&route_urls^[4^]=^%^2Fabugarcia_fishing^%^2Ffollowing^%^2F&routing_namespace=igx_www&__d=www&__user=0&__a=1&__req=1w&__hs=20122.HYP^%^3Ainstagram_web_pkg.2.1...1&dpr=1&__ccg=MODERATE&__rev=1019780606&__s=l0qn91^%^3Aaja29j^%^3A6yq7te&__hsi=7467200991684461158&__dyn=7xeUjG1mxu1syUbFp41twpUnwgU7SbzEdF8aUco2qwJxS0k24o0B-q1ew6ywaq0yE462mcw5Mx62G5UswoEcE7O2l0Fwqo31w9a9wtUd8-U2zxe2GewGw9a361qw8Xxm16wUwtE1wEbUGdG1QwTU9UaQ0Lo6-3u2WE5B08-269wr86C1mwPwUQp1yUb8jK5V89F8uwm8jxK2K2G0EoKmUhw&__csr=gTf0IsQahsn96l8YG4nvbsTi9h9ep4WnvhkAH_qFpqFvBBCWtejQBHVqSXBXKtzpkXaJp9VH-8AAhVKdg-49HGA9XZ2qHh-9haGWVuVUKqh7LzVLK4p8KmpaELVojgDuq8UJ2aXyQUly998G9DyFFpJ6K9DDzpozt6giGfCBy9omw04-ZwadADwq8Mhy88Qdgnypo4Bw5oxeO1O444U3jKm0zobE4Z0Xw2G81zo1To1IU0Fh8UG1vybUBa2iEhAtwJBwzzQ1qwopEpjAg2HU7NxwN9GhNx4940tocoaoW2Q92pe1exC1dCwxxGlj0t411U3JwVwzMw3G06dcw05KOaw11-04y8&__comet_req=7&fb_dtsg=NAcNqgl6_zEKWJfzHXnws7p0Vlg3OiqczVz33l_OcfSnTw1jLF7KUvQ^%^3A17865145036029998^%^3A1738574805&jazoest=26476&lsd=SdlcfsbF31F8J2dnwImpC0&__spin_r=1019780606&__spin_b=trunk&__spin_t=1738593213'
 
 # function to find and access a user profile by its username
-def load_profile_page(username, cookies, headers, data):
+def load_profile_page(username, headers, data):
 
     # Define the URL for the user's profile
     profile_url = f"https://www.instagram.com/{username}/"
@@ -130,11 +100,12 @@ def load_profile_page(username, cookies, headers, data):
 username = "abugarcia_fishing"  # Replace with the actual username you want to search
 
 # Call the function
-load_profile_page(username, cookies, headers, data)
+load_profile_page(username, headers, data)
 
+data = 'av=17841460777147385&__d=www&__user=0&__a=1&__req=1q&__hs=20122.HYP^%^3Ainstagram_web_pkg.2.1...1&dpr=1&__ccg=MODERATE&__rev=1019780606&__s=l0qn91^%^3Aaja29j^%^3A6yq7te&__hsi=7467200991684461158&__dyn=7xeUjG1mxu1syUbFp41twpUnwgU7SbzEdF8aUco2qwJxS0k24o0B-q1ew6ywaq0yE462mcw5Mx62G5UswoEcE7O2l0Fwqo31w9a9wtUd8-U2zxe2GewGw9a361qw8Xxm16wUwtE1wEbUGdG1QwTU9UaQ0Lo6-3u2WE5B08-269wr86C1mwPwUQp1yUb8jK5V89F8uwm8jxK2K2G0EoKmUhw&__csr=gTf0IsQahsn96l8YG4nvbsTi9h9ep4WnvhkAH_qFpqFvBBCWtejQBHVqSXBXKtzpkXaJp9VH-8AAhVKdg-49HGA9XZ2qHh-9haGWVuVUKqh7LzVLK4p8KmpaELVojgDuq8UJ2aXyQUly998G9DyFFpJ6K9DDzpozt6giGfCBy9omw04-ZwadADwq8Mhy88Qdgnypo4Bw5oxeO1O444U3jKm0zobE4Z0Xw2G81zo1To1IU0Fh8UG1vybUBa2iEhAtwJBwzzQ1qwopEpjAg2HU7NxwN9GhNx4940tocoaoW2Q92pe1exC1dCwxxGlj0t411U3JwVwzMw3G06dcw05KOaw11-04y8&__comet_req=7&fb_dtsg=NAcNqgl6_zEKWJfzHXnws7p0Vlg3OiqczVz33l_OcfSnTw1jLF7KUvQ^%^3A17865145036029998^%^3A1738574805&jazoest=26476&lsd=SdlcfsbF31F8J2dnwImpC0&__spin_r=1019780606&__spin_b=trunk&__spin_t=1738593213&fb_api_caller_class=RelayModern&fb_api_req_friendly_name=PolarisProfilePostsQuery&variables=^%^7B^%^22data^%^22^%^3A^%^7B^%^22count^%^22^%^3A12^%^2C^%^22include_reel_media_seen_timestamp^%^22^%^3Atrue^%^2C^%^22include_relationship_info^%^22^%^3Atrue^%^2C^%^22latest_besties_reel_media^%^22^%^3Atrue^%^2C^%^22latest_reel_media^%^22^%^3Atrue^%^7D^%^2C^%^22username^%^22^%^3A^%^22abugarcia_fishing^%^22^%^2C^%^22__relay_internal__pv__PolarisIsLoggedInrelayprovider^%^22^%^3Atrue^%^7D&server_timestamps=true&doc_id=8934560356598281'
 
 # function for accessing id of the latest post in a users profile
-def get_post_id(cookies, headers, data):
+def get_post_id(headers, data):
     # Define the URL
     profile_url = "https://www.instagram.com/graphql/query"
 
@@ -163,11 +134,11 @@ def get_post_id(cookies, headers, data):
 data = """av=17841461002400656&__d=www&__user=0&__a=1&__req=7&__hs=20121.HYP%3Ainstagram_web_pkg.2.1...1&dpr=1&__ccg=MODERATE&__rev=1019772043&__s=4rquec%3Arzn4z9%3Ahvqkfh&__hsi=7466797561635021871&__dyn=7xe5WwlEnwn8K2Wmm1twpUnwgU7S6EeUaUco38w5ux609vCwjE1EE2Cw8G11w6zx62G3i1ywOwa90Fw4Hw9O0M82zxe2GewGw9a361qw8W5U4q09yyES1Twoob82ZwrUdUbGw4mwr86C1mwrd6goK10xKi2K7E5y4UrwHwcObBxm&__csr=&__comet_req=7&fb_dtsg=NAcMX8y-70vfn9Y2yeZt27RlxoaAzjQ4tzgiZuh_HaMSEPGNcYwOrjw%3A17858449030071790%3A1738410193&jazoest=26461&lsd=ThneM-hsSbTaKh7Ina08NP&__spin_r=1019772043&__spin_b=trunk&__spin_t=1738499282&fb_api_caller_class=RelayModern&fb_api_req_friendly_name=PolarisProfilePostsQuery&variables=%7B%22data%22%3A%7B%22count%22%3A12%2C%22include_reel_media_seen_timestamp%22%3Atrue%2C%22include_relationship_info%22%3Atrue%2C%22latest_besties_reel_media%22%3Atrue%2C%22latest_reel_media%22%3Atrue%7D%2C%22username%22%3A%22abugarcia_fishing%22%2C%22__relay_internal__pv__PolarisIsLoggedInrelayprovider%22%3Atrue%7D&server_timestamps=true&doc_id=8934560356598281"""
 
 # Call the function
-last_post_id = get_post_id(cookies, headers, data)
+last_post_id = get_post_id(headers, data)
 
 
 #function for opening
-def open_last_post(last_post_id, cookies, headers, data):
+def open_last_post(last_post_id, headers, data):
     # Define the URL for the last post
     url = f"https://www.instagram.com/p/{last_post_id}"
 
@@ -179,18 +150,15 @@ def open_last_post(last_post_id, cookies, headers, data):
     
     return response
 
-# Example usage
-last_post_id = "DFi3K0jRfwH"  # Replace with the last post ID
-
 # Call the function
-response = open_last_post(last_post_id, cookies, headers, data)
+response = open_last_post(last_post_id, headers, data)
 
 
 # function for liking latest post
-def like_post(url, cookies, headers):
+def like_post(url, headers):
 
     # Define the new referer URL
-    new_referer = 'https://www.instagram.com/p/DFi3K0jRfwH/'
+    new_referer = f'https://www.instagram.com/p/f{last_post_id}/'
 
     # Check if 'Referer' is in the headers and update it, otherwise add it
     if 'Referer' in headers:
@@ -221,5 +189,5 @@ def like_post(url, cookies, headers):
 # Example usage
 url = "https://www.instagram.com//api/v1/web/likes/3559410931047449099/like/"
 
-like_post(url, cookies, headers)
+like_post(url, headers)
     
