a1fec30672a7174ebeb78ae35f0a54b327cd7517
amir movassaghi <movassaghi6@gmail.com>
2025-02-11 11:19:30 +0330

    Added support for updating some fields of header and data based on username

--- app/main.py
+++ app/main.py
@@ -24,13 +24,18 @@ def automation_like_post(username: str):
     # update header values
     extracted_header_values = extract_all_values_from_html(header_patterns, response.text)
     request_header = RequestHeader()
+    ## set Referer based on username
+    request_header.set_referer(f"{username}")
     updated_header =update_model_fields(request_header, extracted_header_values)
 
 
     # update data values
     extracted_data_values = extract_all_values_from_html(data_patterns, response.text)
     request_data = RequestData()
+    ## set username in the variable field 
+    request_data.set_username(f"{username}")
     updated_data =update_model_fields(request_data, extracted_data_values)
+
 
 
     # go to profile
@@ -52,7 +57,7 @@ def automation_like_post(username: str):
 
 
 if __name__ == "__main__":
-    username = ""
+    username = "cristiano"
     automation_like_post(username)
 
 
